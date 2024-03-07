71de8c7908aad2ca1c829b18a9037d8e6c241df7
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-03-07 12:53:40 -0800

    add mode && add clean: true to output

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     devServer: {
         static: './dist',
@@ -30,5 +31,6 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
 };
